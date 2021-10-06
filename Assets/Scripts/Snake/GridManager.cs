cdeac1a19195dd0b2034458235dd366282dd7085
Joey Einerhand <j.einerhand@live.nl>
2021-10-06 12:47:59 +0200

    added knowledge of content to tiles

--- Assets/Scripts/Snake/GridManager.cs
+++ Assets/Scripts/Snake/GridManager.cs
@@ -8,6 +8,7 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int tileWidth, tileHeight;
     [SerializeField] private Tile tilePrefab;
     [SerializeField] private Collider2D areaToFillWithGrid;
+    public List<List<int>> tilesWithContent = new List<List<int>>();
 
     private void Start()
     {
@@ -18,8 +19,8 @@ public class GridManager : MonoBehaviour
         Bounds boundsOfAreaToFill = areaToFillWithGrid.bounds;
         int amountOfTilesHorizontally = (int)(boundsOfAreaToFill.size.x / tileWidth);
         int amountOfTilesVertically = (int)(boundsOfAreaToFill.size.y / tileHeight);
-        float firstTileX = transform.position.x + (tileWidth / 2f) - ((int)(amountOfTilesHorizontally / 2) * tileWidth);
-        float firstTileY = transform.position.y + (tileHeight / 2f) - ((int)(amountOfTilesVertically / 2) * tileHeight);
+        float firstTileX = transform.position.x - (tileWidth / 2f) + ((int)(amountOfTilesHorizontally / 2) * tileWidth);
+        float firstTileY = transform.position.y - (tileHeight / 2f) + ((int)(amountOfTilesVertically / 2) * tileHeight);
         for (int x = 0; x < amountOfTilesHorizontally; x++)
         {
             for (int y = 0; y < amountOfTilesVertically; y++)
@@ -30,6 +31,19 @@ public class GridManager : MonoBehaviour
 
                 Tile spawnedTile = Instantiate(tilePrefab, new Vector2(xToSpawnTileAt, yToSpawnTileAt), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
+                spawnedTile.OwnGridManager = this;
+                spawnedTile.CoordinateInGrid = new Vector2(x, y);
+
+                // if the tile is a boundary, change the tile type to be a wall.
+                if (x == 0 || x == amountOfTilesHorizontally || y == 0 || y == amountOfTilesVertically)
+                {
+                    spawnedTile.ChangeTileContentType(ContentType.wall);
+                }
+                else
+                {
+                    spawnedTile.ChangeTileContentType(ContentType.empty);
+                }
+                
             }
         }
     }
